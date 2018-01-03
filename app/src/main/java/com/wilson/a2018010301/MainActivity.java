01c059f7f9bf9d18cdb96f3a6e20b300429ac526
C1 <c1@gjun.com>
2018-01-03 15:13:56 +0800

    Initial commit

---
+++ app/src/main/java/com/wilson/a2018010301/MainActivity.java
@@ -0,0 +1,30 @@
+package com.wilson.a2018010301;
+
+import android.support.annotation.DrawableRes;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        img = (ImageView) findViewById(R.id.imageView);
+    }
+    public void click1(View v)
+    {
+        img.setImageResource(R.drawable.bird);
+    }
+    public void click2(View v)
+    {
+        Picasso.with(MainActivity.this).load("http://www.pcschool.com.tw/updimg/act/2017RWD_AD/7bAD_945d.jpg")
+                .into(img);
+    }
+}
